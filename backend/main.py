26fff41a042bc0fa33655042bcb24322d2cc2f55
ant <anton-r@live.com>
2021-11-08 21:33:03 +0200

    backend deme ready

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,31 @@ from typing import Optional
 from fastapi import FastAPI
 
 server = FastAPI()
+
+
 # uvicorn backend.main:server --reload
 
 
 @server.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_board():
+    board_data = {
+        'tasks': {
+            'task_1': {'id': 'task_1', 'content': 'content1'},
+            'task_2': {'id': 'task_2', 'content': 'content2'},
+            'task_3': {'id': 'task_3', 'content': 'content3'},
+        },
+        'columns': {
+            'column_1': {
+                'id': 'column_1',
+                'title': 'todo',
+                'tasks_id': ['task_1', 'task_2']
+            },
+            'column_2': {
+                'id': 'column_2',
+                'title': 'done',
+                'tasks_id': ['task_3', ]
+            }
+        },
+        'column_order': ['column_1', 'column_2']
+    }
+    return {"boards": board_data}
