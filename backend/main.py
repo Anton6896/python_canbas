0ee9cad0c0c7245e5920ca1d1a23b80f842eae99
ant <anton-r@live.com>
2021-11-08 21:21:09 +0200

    requirements

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,3 @@ server = FastAPI()
 @server.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@server.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-# requirements.txt
